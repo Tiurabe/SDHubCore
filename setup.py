55c521949ddf72f98292b992897f00058010afcb
Tiurabe <63129379+Tiurabe@users.noreply.github.com>
2024-01-30 16:33:30 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(
     description='Several functions and tools used by SDHub',
     url='https://github.com/tiurabe/SDHubCore',
     packages=find_packages(),
-    install_requires=['requests==2.31.0', 'subprocess', "pydantic==2.6.0", "colored==2.2.4"],
+    install_requires=['requests==2.31.0', "pydantic==2.6.0", "colored==2.2.4"],
 )
